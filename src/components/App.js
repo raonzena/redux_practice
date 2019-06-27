3bb4ace7c0ceaa50765d227ff7b1f05a99cf57f4
raonzena <seven1004z@gmail.com>
2019-06-27 11:03:57 +0900

    modify redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from 'react'
+import Footer from './Footer'
+import AddTodo from '../containers/AddTodo'
+import VisibleTodoList from '../containers/VisibleTodoList'
 
-const App = () => {
-  return (
-    <div>
-      <p>React here!</p>
-    </div>
-  )
-};
+const App = () => (
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import App from './containers/App';
+import todoApp from './reducers';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+let store = createStore(todoApp);
+
+let rootElement = document.getElementById('root');
+React.render(
+  // React 0.13의 이슈를 회피하기 위해 
+  // 반드시 함수로 감싸줍니다.
+  <Provider store={store}>
+    {() => <App />}
+  </Provider>,
+  rootElement
+);
